5abc84b890bfa708fe5507e4180d086a7a111c72
Reed Brooks <reedb36@gmail.com>
2020-07-27 10:35:27 -0700

    Initial listener handling

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("Testing...");
+console.log("Initializing...");
+
+const xTurn= true;
+const playerPieces = ['X','O'];
+
+//function declarations
+
+//Check if current position is empty (-).  If so, set to the piece of the active player
+//and switch to the next side's turn
+function setPiece(event) {
+  console.log("Piece clicked");
+}
+
+//resets board to initial state
+function clearBoard() {
+  console.log("Placeholder");
+  //remove all pieces
+  //set game to start with player X
+  xTurn = true;
+}
+
+//event handler declarations
+
+document.getElementById("cell1").addEventListener("click", setPiece);
+document.getElementById("cell2").addEventListener("click", setPiece);
+document.getElementById("cell3").addEventListener("click", setPiece);
+document.getElementById("cell4").addEventListener("click", setPiece);
+document.getElementById("cell5").addEventListener("click", setPiece);
+document.getElementById("cell6").addEventListener("click", setPiece);
+document.getElementById("cell7").addEventListener("click", setPiece);
+document.getElementById("cell8").addEventListener("click", setPiece);
+document.getElementById("cell9").addEventListener("click", setPiece);
+
+console.log("Script loaded.");
