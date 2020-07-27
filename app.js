f789e937ec58239888ae2ccea44b206fb0b7e7dc
Reed Brooks <reedb36@gmail.com>
2020-07-27 11:12:53 -0700

    Work on checkWinner function, implement detection for draws

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log("Initializing...");
 
 let xTurn= true;
 const playerPieces = ['X','O'];
+let piecesPlaced = 0;
 
 //function declarations
 
@@ -11,9 +12,11 @@ function setPiece(event) {
   console.log("Piece clicked");
   if(xTurn && event.target.innerHTML === '-')  {
     event.target.innerHTML = 'X';
+    piecesPlaced++;
     xTurn = !xTurn;
   } else if(event.target.innerHTML === '-'){
     event.target.innerHTML = 'O';
+    piecesPlaced++;
     xTurn = !xTurn;
   }
   //check for a winner
@@ -23,6 +26,8 @@ function setPiece(event) {
 //resets board to initial state
 function clearBoard() {
   //remove all pieces
+  piecesPlaced = 0;
+
   let cells = document.getElementsByTagName('td');
   for(let i = 0; i < cells.length; i++){
     cells[i].innerHTML = '-';
@@ -35,12 +40,27 @@ function clearBoard() {
 
 //Run after every piece is placed to check for a winner.
 //If a player wins, update the reset element with text informing the players who won.
+//If a tie, also inform the users
 function checkWinner() {
   console.log("Checking for a winner...");
+  let winnerFound = false;
+  let cells = document.getElementsByTagName('td');
+  //check to see if a player has won.  Must check:
+  //rows (123 / 456 / 789)
+  //columns (147 / 258 / 369)
+  //diagonals (159 / 357)
+
+  if(winnerFound) {
+    console.log("Winner winner chicken dinner!");
+  } else if (piecesPlaced >= 9) {
+    console.log("It's a draw!");
+    document.getElementById('reset').innerHTML = "It's a draw!  Play again?";
+  } else {
+    console.log("Keep going...");
+  }
 }
 
 //event handler declarations
-
 document.getElementById("cell1").addEventListener("click", setPiece);
 document.getElementById("cell2").addEventListener("click", setPiece);
 document.getElementById("cell3").addEventListener("click", setPiece);
